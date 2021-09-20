52e500b8372718f9c14725b2ca09269d8fc3f387
Ahmad-GEN <ahmad.abdulrehman@geniteam.pk>
2021-09-20 17:54:18 +0500

    Initialize project

---
+++ Assets/Scripts/audio_Script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audio_Script : MonoBehaviour
+{
+    public AudioSource myFx;
+    public AudioClip hoverFx;
+    public AudioClip clickFx;
+
+    public void hoverSound()
+    {
+        myFx.PlayOneShot(hoverFx);
+    }
+
+    public void clickSound()
+    {
+        myFx.PlayOneShot(clickFx);
+    }
+
+}
---
+++ Assets/Scripts/changeScreen.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScreen : MonoBehaviour
+{
+   public void onClick_ChangeScreen(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+}
